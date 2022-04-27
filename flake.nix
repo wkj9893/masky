f3dce1e88a846191ccdae5132fe6b432fc420b50
wkj <wkj9893@gmail.com>
2022-04-27 23:15:37 +0800

    use flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "masky flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system: rec {
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [ go_1_18 golangci-lint ];
+      };
+    });
+}
